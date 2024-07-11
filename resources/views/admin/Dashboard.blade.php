f444529f37e08fe3ff4498acd413b6eb043317c8
El Rakkai Omar Wahid <ggg153563@gmail.com>
2024-07-11 15:09:56 +0700

    dashbiard

--- resources/views/admin/Dashboard.blade.php
+++ resources/views/admin/Dashboard.blade.php
@@ -1,4 +1,50 @@
 @extends('layout')
 @section('content')
-    <p>haloooo</p>
+    <section class="box-border p-0 m-0 bg-slate-100 text-center justify-center items-center h-screen">
+        <div class="flex gap-[1.5rem] pt-[3rem] pb-[1rem] px-[5rem] bg-slate-300">
+            <div class="bg-slate-400 w-[14rem] h-[10rem] block justify-center rounded-[15px]">
+                <div>
+                    <i class='bx bx-sushi'></i>
+                </div>
+                p
+            </div>
+            <div class="bg-slate-400 w-[14rem] h-[10rem] block justify-center rounded-[15px]">
+                <div>
+                    <i class='bx bx-sushi'></i>
+                </div>
+                p
+            </div>
+            <div class="bg-slate-400 w-[14rem] h-[10rem] block justify-center rounded-[15px]">
+                <div>
+                    <i class='bx bx-sushi'></i>
+                </div>
+                p
+            </div>
+            <div class="bg-slate-400 w-[14rem] h-[10rem] block justify-center rounded-[15px]">
+                <div>
+                    <i class='bx bx-sushi'></i>
+                </div>
+                p
+            </div>
+        </div>
+        <div class="flex">
+            <div class="block px-[5rem]">
+                <div class="w-[35rem] h-[5rem] bg-slate-200 rounded-[10px] my-[0.8rem]">
+                    p
+                </div>
+                <div class="w-[35rem] h-[5rem] bg-slate-200 rounded-[10px] my-[0.8rem]">
+                    p
+                </div>
+                <div class="w-[35rem] h-[5rem] bg-slate-200 rounded-[10px] my-[0.8rem]">
+                    p
+                </div>
+                <div class="w-[35rem] h-[5rem] bg-slate-200 rounded-[10px] my-[0.8rem]">
+                    p
+                </div>
+            </div>
+            <div class="bg-cyan-200 w-[20rem] my-[1rem] ">
+                p
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite('resources/css/app.css')
 </head>
 <body>
     @yield('content')

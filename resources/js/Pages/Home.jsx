2fc540608502d143416e6232a08737832e1a2901
algazza <fadhilaswadira72564@gmail.com>
2024-07-20 20:34:55 +0700

    form

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import FormBank from "@/Components/FormBank";
-import { formIdentitas } from "@/data";
-import Kredit from "@/Components/Form/Kredit";
 import Tabungan from "@/Components/Form/Tabungan";
 import styles from "@/data/style";
 import Footer from "@/Layouts/Footer";
@@ -12,7 +10,6 @@ const Home = () => {
 <>
             <main className="bg-primary overflow-hidden font-jakarta">
                 <div className={`${styles.flexCenter}`}>
-                    {/* <FormBank isiPenting={<Kredit />} value={""} routes={"{{route}}"}/> */}
                     <FormBank isiPenting={<Tabungan />} value={""} routes={"{{route}}"}/>
                 </div>
 
